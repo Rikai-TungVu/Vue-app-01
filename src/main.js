c42f45234a13072199b42f32fc74c3fd9bd290dd
Rikai-TungVu <tung.vu@rikai.technology>
2023-01-13 11:02:38 +0900

    new commit about newfriend add

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 import { createApp } from 'vue';
 
 import App from './App.vue';
+import NewFriend from './components/NewFriend.vue'
 import FriendContact from './components/FriendContact.vue';
-/* Add file FriendContact vào file main*/
+/* Import file FriendContact vào file main*/
 import './components/FriendStyle.css';
 /* Add dự liệu đồ họa */
 
 const app = createApp(App);
 
 app.component('friend-contact', FriendContact);
+/* Khai báo thêm element mới  friend-contact từ file FriendContact*/
+app.component('new-friend', NewFriend);
 
 app.mount('#app');
